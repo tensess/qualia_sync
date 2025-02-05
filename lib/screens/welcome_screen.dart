1eb935bcc2f6dd6c4391dd8f327f209c41329d2b
tensess <tensess.offline@gmail.com>
2025-02-05 01:13:23 +0100

    feat: add Sign in and Sign up buttons

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -5,20 +5,49 @@ class WelcomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Qualia Sync'),
-      ),
-      body: Center(
-        child: Text(
-          'Qualia Sync',
-          style: TextStyle(
-            fontSize: 36,
-            fontWeight: FontWeight.bold,
-            color: Colors.blue,
-          ),
+    final List<SocialButtons> buttons = [
+      SocialButtons('Sign in', () {}),
+      SocialButtons('Sign up', () {}),
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+        body: Stack(
+          children: [
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  const Text(
+                    'Qualia Sync',
+                    style: TextStyle(
+                      fontSize: 36,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 100,
+                  ),
+                  ...buttons.map((element) => Padding(
+                      padding: const EdgeInsets.only(bottom: 10),
+                      child: ElevatedButton(
+                        onPressed: element.onPressed,
+                        child: Text(element.text),
+                      )))
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
+}
+
+class SocialButtons {
+  final String text;
+  final VoidCallback onPressed;
+
+  SocialButtons(this.text, this.onPressed);
 }
