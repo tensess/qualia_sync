2e0c3600b0f91efdcb58360149fb953ab7073393
tensess <tensess.offline@gmail.com>
2025-02-06 16:32:45 +0100

    chore: remove desktop platforms from the project

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const WelcomeScreen(), // Используем новый экран
+      home: const WelcomeScreen(),
     );
   }
 }
